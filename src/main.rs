4c9a0b191fdc63bc0ecd10390a1eb0e8ce3e2726
Ragnt <rs.butler@protonmail.com>
2024-01-27 19:24:03 -0500

    test monitor mode

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ fn main() {
     let mut socket: Nl80211 = nl80211_ng::Nl80211::new().unwrap();
     let ifaces = socket.get_interfaces();
 
-    for iface in ifaces {
-        println!("{}", iface.1.pretty_print());
+    for iface in ifaces.clone() {
+        println!("{}\n{}", iface.0,iface.1.pretty_print());
+        let _ = socket.set_interface_monitor(false, iface.1.index.unwrap());
     }
 }
